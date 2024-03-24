86a358b18b64209475d3e8d496f1bdfb36caaed2
Qubx3310 <kierownik.anpol@gmail.com>
2024-03-24 09:49:58 +0100

    add hotel entity

---
+++ src/main/java/entity/Hotel.java
@@ -0,0 +1,25 @@
+package entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "hotel")
+public class Hotel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "Hotel")
+    private Long id;
+    @Column(name = "hotelName")
+    private String hotelName;
+    @Column(name = "standard")
+    private String standard;
+    @Column(name = "description")
+    private String description;
+    @Column(name = "CityId")
+    private Long cityId;
+
+
+
+
+
+}
